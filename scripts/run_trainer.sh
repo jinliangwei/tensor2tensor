c42797065738504d7f032d0a13f2dd5190bfb20f
jinliangwei <jinliangwei1@gmail.com>
2018-11-02 14:43:55 -0400

    added scripts and conf to facilitate running distributed T2T experiments; see scripts/run.py for more details

---
+++ scripts/run_trainer.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+PROBLEM=languagemodel_lm1b32k
+TMP_DIR=/datasets/BigLearning/jinlianw/tmp
+DATA_DIR=/datasets/BigLearning/jinlianw/languagemodel_lm1b32k
+
+MODEL=attention_lm_moe
+HPARAMS=attention_lm_no_moe_small
+TRAIN_STEPS=400
+
+TRAIN_DIR=/proj/BigLearning/jinlianw/t2t_train_single/$PROBLEM/$MODEL-$HPARAMS
+
+mkdir -p $TRAIN_DIR
+
+DBG_PROFILE=false
+
+USE_NVPROF=false
+
+if [ $USE_NVPROF == "true" ]
+then
+    /usr/local/cuda/bin/nvprof \
+	--export-profile profile.nvvp \
+	-f --print-summary \
+	./tensor2tensor/bin/t2t-trainer \
+	--data_dir=$DATA_DIR \
+	--tmp_dir=$TMP_DIR \
+	--problem=$PROBLEM \
+	--model=$MODEL \
+	--hparams_set=$HPARAMS \
+	--output_dir=$TRAIN_DIR \
+	--train_steps=$TRAIN_STEPS \
+	--dbgprofile=$DBG_PROFILE
+else
+    ./tensor2tensor/bin/t2t-trainer \
+	--data_dir=$DATA_DIR \
+	--tmp_dir=$TMP_DIR \
+	--problem=$PROBLEM \
+	--model=$MODEL \
+	--hparams_set=$HPARAMS \
+	--output_dir=$TRAIN_DIR \
+	--train_steps=$TRAIN_STEPS \
+	--dbgprofile=$DBG_PROFILE
+fi
