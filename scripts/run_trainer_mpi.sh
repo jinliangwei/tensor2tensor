5dd0e215dc678571f84ddab223ae3892763e4aa4
Aurick Qiao <qiao@aurick.net>
2019-07-01 08:46:13 -0600

    update train script

--- scripts/run_trainer_mpi.sh
+++ scripts/run_trainer_mpi.sh
@@ -6,13 +6,14 @@ DATA_DIR=/datasets/BigLearning/aqiao/languagemodel_lm1b8k_packed
 
 MODEL=mtf_transformer
 HPARAMS=mtf_transformer_tiny_lm_moe
-TRAIN_STEPS=100000
+TRAIN_STEPS=1000
 
 MESH_SIZE=$OMPI_COMM_WORLD_SIZE
 #MESH_SHAPE="batch:1;model:$MESH_SIZE"
 #LAYOUT="batch:batch;vocab:model;d_ff:model;heads:model;experts:model"
 MESH_SHAPE="all:$MESH_SIZE"
 LAYOUT="batch:all;experts:all"
+BATCH_SIZE=200
 NUM_EXPERTS=80
 
 TRAIN_DIR=/proj/BigLearning/aqiao/t2t_train_single/$PROBLEM/$MODEL-$HPARAMS
@@ -50,12 +51,14 @@ if [ $OMPI_COMM_WORLD_RANK -eq 0 ]; then
     --problem=$PROBLEM \
     --model=$MODEL \
     --hparams_set=$HPARAMS \
-    --hparams="mesh_shape=$MESH_SHAPE,layout=$LAYOUT,moe_num_experts=$NUM_EXPERTS" \
+    --hparams="mesh_shape=$MESH_SHAPE,layout=$LAYOUT,moe_num_experts=$NUM_EXPERTS,batch_size=$BATCH_SIZE" \
     --output_dir=$TRAIN_DIR \
     --train_steps=$TRAIN_STEPS &
 fi
 
 if [ $OMPI_COMM_WORLD_RANK -lt $MESH_SIZE ]; then
+  export TF_MEM_LOGGER_PATH_PREFIX=/users/aqiao/moe/tensor2tensor/memlog
+  export LD_LIBRARY_PATH=/users/aqiao/cuda-9.0/lib64:$LD_LIBRARY_PATH
   TF_CONFIG=$TF_CONFIG', "task": {"type": "ps", "index": '$OMPI_COMM_WORLD_RANK'}}' \
   ./tensor2tensor/bin/t2t-trainer \
     --schedule=run_std_server \
@@ -64,7 +67,7 @@ if [ $OMPI_COMM_WORLD_RANK -lt $MESH_SIZE ]; then
     --problem=$PROBLEM \
     --model=$MODEL \
     --hparams_set=$HPARAMS \
-    --hparams="mesh_shape=$MESH_SHAPE,layout=$LAYOUT,moe_num_experts=$NUM_EXPERTS" \
+    --hparams="mesh_shape=$MESH_SHAPE,layout=$LAYOUT,moe_num_experts=$NUM_EXPERTS,batch_size=$BATCH_SIZE" \
     --output_dir=$TRAIN_DIR \
     --train_steps=$TRAIN_STEPS
 fi
